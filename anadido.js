e8262b819f104620e9ad56087b6cef2047eb8425
tvfj <74503992+tvfj@users.noreply.github.com>
2024-03-09 19:49:59 +0100

    Update anadido.js

--- anadido.js
+++ anadido.js
@@ -1,4 +1,4 @@
 var img = document.createElement("img");
 img.src = "https://raw.githubusercontent.com/tvfj/eurolive/main/FONDO.png";
-var src = document.getElementById(":root");
+var src = document.getElementById("div");
 src.appendChild(img);
