945366506356451146c5b9632b7675b0f5965b15
tvfj <74503992+tvfj@users.noreply.github.com>
2024-03-09 19:52:28 +0100

    Update anadido.js

--- anadido.js
+++ anadido.js
@@ -2,3 +2,6 @@ var img = document.createElement("img");
 img.src = "https://raw.githubusercontent.com/tvfj/eurolive/main/FONDO.png";
 var src = document.getElementById("div");
 src.appendChild(img);
+
+var p = document.getElementById("hl-name")
+hl-name.innerText = hl-name.innerText+" And this is addon."
